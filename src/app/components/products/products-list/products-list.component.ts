775519fd3220fa3b2fd80c7fa7bc909430a26f00
balajivelu147 <balajimc22@gmail.com>
2021-10-28 18:25:16 +0530

    temp

--- src/app/components/products/products-list/products-list.component.ts
+++ src/app/components/products/products-list/products-list.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
+import { first, take, takeUntil } from 'rxjs/operators';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss'],
-  
+
 })
 export class ProductsListComponent implements OnInit {
 
@@ -15,18 +17,27 @@ export class ProductsListComponent implements OnInit {
 
   ngOnInit(): void {
 
-    this.productsService.getProductsList().subscribe(products => {
+    this.productsService.getProductsList().pipe(
+      // async // used in html template 
+      // first(), // closes subscription once we receive data
+      // take(5), // unsubcribes after 5 subscription
+      //takeUntill(), // unlimited subscription
+      //takeWhile(),
+    ).subscribe(products => {
       this.productsList = products;
       this.productListFiltered = this.productsList;
       console.log(this.productsList)
     });
+
+    //this.productsService.getEmployeeWithSalary().subscribe(console.log);
+    this.productsService.getSalaryByEmployeeId(2).subscribe(console.log);
   }
-  filterByTextFn(event: any){
-    this.productListFiltered = this.productsList.filter((x: any) =>  x.title.includes(event));
+  filterByTextFn(event: any) {
+    this.productListFiltered = this.productsList.filter((x: any) => x.title.includes(event));
   }
 
   readFilter(event: any) {
-    this.productListFiltered = this.productsList.filter((x : any)=> x.category === event);
+    this.productListFiltered = this.productsList.filter((x: any) => x.category === event);
   }
 
 }
