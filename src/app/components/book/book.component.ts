57d35857e34e166a87a1686fab7c0a26f13c1a7c
vamshishekar <you@example.com>
2021-11-12 18:25:44 +0530

    subject

--- src/app/components/book/book.component.ts
+++ src/app/components/book/book.component.ts
@@ -1,39 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { BookService } from '../../services/book.service';
 
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
 })
 export class BookComponent implements OnInit {
-
   public isSaved: boolean = false;
-  constructor(private bookService: BookService, private acitvateRoute: ActivatedRoute) { }
+  constructor(
+    private bookService: BookService,
+    private acitvateRoute: ActivatedRoute,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-try {
-  console.log(this.acitvateRoute.snapshot.data);
-  // this.bookService.login().subscribe((x: any) => sessionStorage.setItem('login',x.token));
-  this.bookService.getBooks().pipe().subscribe(x=> {
-
-
-  });
-}catch(e){
-  console.log(e);
-}
-
+    this.authService.getAttempts().subscribe(console.log);
     
+    try {
+      console.log(this.acitvateRoute.snapshot.data);
+      // this.bookService.login().subscribe((x: any) => sessionStorage.setItem('login',x.token));
+      this.bookService
+        .getBooks()
+        .pipe()
+        .subscribe((x) => {});
+    } catch (e) {
+      console.log(e);
+    }
   }
 
   checkoutSomething() {
-    this.isSaved =true ;
-    this.bookService.postCheckout(12,{}).subscribe();
+    this.authService.incrementAttempts();
+    this.isSaved = true;
+    this.bookService.postCheckout(12, {}).subscribe();
   }
 
   // confirm(){
 
   // }
-
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,15 +1,30 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   isAuthentic: boolean = false;
-  constructor(private http: HttpClient) { }
+  count: number = 0;
+  // authAttemptsSubject = new Subject();
+  authAttemptsSubject = new BehaviorSubject<number>(8);
+  constructor(private http: HttpClient) {}
 
   isAuthenticated() {
-    return this.http.post<any>('https://bookcart.azurewebsites.net/api/login', {"username":"balajimohan","password":"Balajivelu147"});
+    return this.http.post<any>('https://bookcart.azurewebsites.net/api/login', {
+      username: 'balajimohan',
+      password: 'Balajivelu147',
+    });
     // return this.isAuthentic;
+  }
+
+  incrementAttempts() {
+    this.authAttemptsSubject.next(this.count++);
+  }
+
+  getAttempts() {
+    return this.authAttemptsSubject.asObservable();
   }
 }
